e7c7b56cd856bc8127b7d83f21865359f9552fb5
Vyacheslav Guzev <wcbash@icloud.com>
2023-09-30 11:49:30 +0300

    (*) Add Transform2 extended methods

--- src/core/math/transform2d.h
+++ src/core/math/transform2d.h
@@ -1,29 +1,60 @@
-#pragma once
-
-#include "vector2.h"
-
 template<typename T>
 class Transform2D
 {
 public:
-    Transform2D()
+    Transform2D() : mRotation(0), mTranslation(0, 0) {}
+
+    Transform2D(T aRotation, Vector2<T, T> aTranslation)
+        : mRotation(aRotation), mTranslation(aTranslation) {}
+
+    Vector2<T, T> operator*(const Vector2<T, T>& aV) const
     {
-        mMatrix[0] = Vector2<T>(1.0, 0.0);
-        mMatrix[1] = Vector2<T>(0.0, 1.0);
+        T newX = aV.mX * cos(mRotation) - aV.mY * sin(mRotation) + mTranslation.mX;
+        T newY = aV.mX * sin(mRotation) + aV.mY * cos(mRotation) + mTranslation.mY;
+        return Vector2<T, T>(newX, newY);
     }
 
-    // Transform2D(const double aRotation, const Vector2D &aScale, const double aTilt, )
-
-    Transform2D(const Vector2<T>& aX, const Vector2<T>& aY, const Vector2& aVec)
+    Transform2D& operator*=(const Transform2D& aT)
     {
-        mMatrix[0] = aX;
-        mMatrix[1] = aY;
-        mMatrix[2] = aVec;
+        mRotation += aT.mRotation;
+        mTranslation += aT.mTranslation;
+        return *this;
     }
 
-    void scale(const Vector2D &p_scale);
-    
+    void scale(const Vector2<T, T>& aScale)
+    {
+        mRotation *= aScale.len();
+        mTranslation *= aScale;
+    }
+
+    void rotate(T aAngle)
+    {
+        mRotation += aAngle;
+    }
+
+    void translate(const Vector2<T, T>& aOffset)
+    {
+        mTranslation += aOffset;
+    }
+
+    Transform2D inverse() const
+    {
+        T invRotation = -mRotation;
+        Vector2<T, T> invTranslation = Vector2<T, T>(-mTranslation.mX, -mTranslation.mY);
+        return Transform2D(invRotation, invTranslation);
+    }
+
+    Transform2D interpolate_with(const Transform2D& aTransform, T aWeight) const
+    {
+        T newRotation = mRotation * (1 - aWeight) + aTransform.mRotation * aWeight;
+        Vector2<T, T> newTranslation = mTranslation * (1 - aWeight) + aTransform.mTranslation * aWeight;
+        return Transform2D(newRotation, newTranslation);
+    }
 
 private:
-    Vector2<T> mMatrix[3];
+    T mRotation;
+    Vector2<T, T> mTranslation;
 };
+
+using Transform2Dd = Transform2D<double>;
+using Transform2Df = Transform2D<float>;
